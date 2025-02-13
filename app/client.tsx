621d27c090299c010f76aea1658b6f0d882b62dc
Julia Mendes <juliamendesc@gmail.com>
2025-02-13 20:21:37 +0000

    config clerk auth

---
+++ app/client.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ClerkProvider } from "@clerk/nextjs"
+
+export default function ClientWrapper({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import ClientWrapper from "./client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientWrapper>
+          {children}
+        </ClientWrapper>
       </body>
     </html>
   );
